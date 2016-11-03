093620f69de5809dc4fb04ab0dce2833267e4c92
Moritz Moeller <mm@mxs.de>
2016-11-03 17:51:28 +0100

    ..

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 module.exports = function expressModifyResponse(checkCallback, modifyCallback)
 {
     return function expressModifyResponse(req, res, next) {
+        console.log('WORKS?');
         var _end = res.end;
         var _write = res.write;
+        var checked = false;
         var buffers = [];
         var addBuffer = (chunk, encoding) => {
             if (chunk === undefined) return;
@@ -12,12 +14,14 @@ module.exports = function expressModifyResponse(checkCallback, modifyCallback)
             buffers.push(chunk);
         };
         res.write = function write(chunk, encoding) {
-            if (!res.headersSent) {
+            if (!checked) {
+                console.log('WRITE CHECK');
+                checked = true;
                 var hook = checkCallback(req, res);
                 if (!hook) {
                     res.end = _end;
                     res.write = _write;
-                    res.write(chunk, encoding);
+                    return res.write(chunk, encoding);
                 } else {
                     addBuffer(chunk, encoding);
                 }
@@ -26,7 +30,21 @@ module.exports = function expressModifyResponse(checkCallback, modifyCallback)
             }
         };
         res.end = function end(chunk, encoding) {
-            addBuffer(chunk, encoding);
+            if (!checked) {
+                console.log('END CHECK');
+                checked = true;
+                var hook = checkCallback(req, res);
+                if (!hook) {
+                    res.end = _end;
+                    res.write = _write;
+                    return res.end(chunk, encoding);
+                } else {
+                    addBuffer(chunk, encoding);
+                }
+            } else {
+                addBuffer(chunk, encoding);
+            }
+            console.log('ASDASD');
             var buffer = Buffer.concat(buffers);
             Promise.resolve(modifyCallback(req, res, buffer)).then((result) => {
                 if (res.getHeader('Content-Length')) {
